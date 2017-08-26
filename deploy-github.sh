f2a647631b35e256c3d419d6af74cadbe81a954a
Emanuel G <emanuelgdesouza@gmail.com>
2017-08-26 10:43:38 -0300

    Update deploy to Github source and README

--- deploy-github.sh
+++ deploy-github.sh
@@ -3,7 +3,7 @@ yarn run deploy-github
 cd dist
 rm -rf .git/
 git init
-git remote add origin "https://github.com/emanuelgsouza/debate3d.git"
+git remote add origin "https://github.com/emanuelgsouza/debate3d-site.git"
 git add .
 git commit -am "deploy"
 git push origin master:gh-pages --force

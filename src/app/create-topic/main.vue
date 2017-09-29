be810c1816d33bb7c93065999e77e82ae77302c0
Emanuel G <emanuelgdesouza@gmail.com>
2017-09-29 00:12:21 -0300

    Update form to create topic

--- src/app/create-topic/main.vue
+++ src/app/create-topic/main.vue
@@ -1,18 +1,22 @@
 <script>
+  import { isEmpty } from 'lodash'
+  import { prop } from 'ramda'
   import PositionsQuery from './positions.gql'
-  import { isEmpty, uniq } from 'lodash'
-  import Autocomplete from './autocomplete.vue'
+  import createTopicMutation from './create-topic-mutation.gql'
+  import addTags from './add-tags/main'
 
   export default {
     name: 'create-topic',
-    components: { Autocomplete },
+    components: { addTags },
     data () {
       return {
         positions: [],
         id_position: '',
         content: '',
         title: '',
-        selected: []
+        selected: [],
+        isActive: false,
+        selectedTags: []
       }
     },
     apollo: {
@@ -24,18 +28,31 @@
     },
     methods: {
       submit () {
+        this.$apollo.mutate({
+          mutation: createTopicMutation,
+          variables: {
+            topic: this.topic
+          }
+        }).then(console.log).catch(console.error)
       },
-      setSelected (value) {
-        if (!isEmpty(value)) {
-          this.selected.push(value)
-          this.selected = uniq(this.selected)
-          return
-        }
+      getSelects (tags) {
+        this.selectedTags = [ ...tags ]
       }
     },
     computed: {
       isFormValid () {
         return !isEmpty(this.content) && !isEmpty(this.id_position) && !isEmpty(this.title)
+      },
+      tags () {
+        return this.selectedTags.map(prop('uid'))
+      },
+      topic () {
+        return {
+          title: this.title,
+          content: this.content,
+          id_position: this.id_position,
+          tag: this.selectedTags
+        }
       }
     }
   }
@@ -46,46 +63,59 @@
     <h1 class="title has-text-centered"> Criar tópico </h1>
     <hr>
 
-    <form action="#" class="app-form" @submit.prevent="submit">
-      <b-field
-        label="Título">
-        <b-input placeholder="Digite um titulo" v-model="title" type="text"/>
-      </b-field>
+    <b-field
+      label="Título">
+      <b-input
+        maxlength="45"
+        placeholder="Digite um titulo"
+        v-model="title"
+        type="text"/>
+    </b-field>
 
-      <b-field
-        label="Conteúdo">
-        <b-input maxlength="1000" type="textarea" v-model="content"></b-input>
-      </b-field>
+    <b-field
+      label="Conteúdo">
+      <b-input maxlength="2000" type="textarea" v-model="content"></b-input>
+    </b-field>
 
-      <b-field
-        label="Qual dicotomia quer para o seu tema?">
-        <b-select
-          placeholder="Dicotomias"
-          v-model="id_position"
-          expanded>
-          <option
-            v-for="position in positions"
-            :value="position.id"
-            :key="position.id">
-            {{ position.positive }} | {{ position.negative }}
-          </option>
-        </b-select>
-      </b-field>
+    <b-field
+      label="Qual dicotomia quer para o seu tema?">
+      <b-select
+        placeholder="Dicotomias"
+        v-model="id_position"
+        expanded>
+        <option
+          v-for="position in positions"
+          :value="position.id"
+          :key="position.id">
+          {{ position.positive }} | {{ position.negative }}
+        </option>
+      </b-select>
+    </b-field>
 
-      <autocomplete
-        label="Taggeie seu tópico - 1° tag"
-        @select="setSelected"/>
-      <autocomplete
-        label="Taggeie seu tópico - 2° tag"
-        @select="setSelected"/>
-      <autocomplete
-        label="Taggeie seu tópico - 3° tag"
-        @select="setSelected"/>
+    <b-field
+      label="Tags selecionadas">
+      <b-taglist v-if="selectedTags.length > 0">
+        <b-tag
+          v-for="tag in selectedTags" :key="tag.uid"
+          type="is-primary"
+          size="is-medium"> {{ tag.label }} </b-tag>
+      </b-taglist>
+    </b-field>
 
-      <button
-        class="button is-success is-medium"
-        :disabled="!isFormValid"> Criar tópico </button>
-    </form>
+    <hr>
+
+    <button
+      class="button is-medium"
+      @click="isActive = true"> Adicionar tags </button>
+
+    <add-tags
+      :isActive.sync="isActive"
+      @get-select="getSelects" />
+
+    <button
+      class="button is-success is-medium"
+      :disabled="!isFormValid"
+      @click="submit"> Criar tópico </button>
   </div>
 </template>
 

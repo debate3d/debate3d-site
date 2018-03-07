794ab71223e79bafe243c07626dcae834f7b9228
EmanuelGSouza <emanuelgdesouza@gmail.com>
2018-03-07 00:06:15 -0300

    update icons to pwa configuration

--- quasar.conf.js
+++ quasar.conf.js
@@ -73,36 +73,20 @@ module.exports = function (ctx) {
     pwa: {
       cacheExt: 'js,html,css,ttf,eot,otf,woff,woff2,json,svg,gif,jpg,jpeg,png,wav,ogg,webm,flac,aac,mp4,mp3',
       manifest: {
-        // name: 'Quasar App',
-        // short_name: 'Quasar-PWA',
-        // description: 'Best PWA App in town!',
+        name: 'Debate3D App',
+        short_name: 'Debate3D App',
         display: 'standalone',
         orientation: 'portrait',
-        background_color: '#ffffff',
-        theme_color: '#027be3',
+        background_color: '#3a4752',
+        theme_color: '#3a4752',
         icons: [
           {
-            'src': 'statics/icons/icon-128x128.png',
-            'sizes': '128x128',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-192x192.png',
+            'src': 'statics/icons/android-chrome-192x192.png',
             'sizes': '192x192',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-256x256.png',
-            'sizes': '256x256',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-384x384.png',
-            'sizes': '384x384',
-            'type': 'image/png'
-          },
-          {
-            'src': 'statics/icons/icon-512x512.png',
+            'src': 'statics/icons/android-chrome-512x512.png',
             'sizes': '512x512',
             'type': 'image/png'
           }
